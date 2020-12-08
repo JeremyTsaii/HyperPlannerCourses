2fed3bb15eae8ab1818deed4956ae385f14dd5d5
Jeremy <jetsai@hmc.edu>
2020-12-07 16:09:24 -0800

    Implement reading all courses

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"encoding/json"
+	"log"
+	"os"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"os"
 )
 
 // Course is struct for a course entry
@@ -28,25 +29,23 @@ func main() {
 		panic(err)
 	}
 
-	// Ping the primary
-	if err := client.Ping(context.TODO(), nil); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected and pinged.")
-
 	// Connect to db
 	db := client.Database("HyperPlanner")
 	courses := db.Collection("Courses")
 
 	// Insert course
-	testCourse := Course{"hmc", "CSCI134", 3.0, "Operating Systems"}
-	insertCourse(courses, testCourse)
+	// testCourse := Course{"hmc", "CSCI134", 3.0, "Operating Systems"}
+	// insertCourse(courses, testCourse)
 
 	// Read course
-	var course bson.M
-	readCourse(courses, course)
+	// var course bson.M
+	// readCourse(courses, course)
+	var results []Course
+	readAllCourses(courses, results)
+
 }
 
+// Insert given course into db
 func insertCourse(courses *mongo.Collection, course Course) {
 	result, err := courses.InsertOne(
 		context.Background(),
@@ -57,9 +56,9 @@ func insertCourse(courses *mongo.Collection, course Course) {
 	}
 
 	fmt.Println("API Result:", result.InsertedID)
-	fmt.Println("Successfully inserted.")
 }
 
+// Read single course from db
 func readCourse(courses *mongo.Collection, course bson.M) {
 	err := courses.FindOne(context.Background(), bson.M{}).Decode(&course)
 
@@ -68,5 +67,34 @@ func readCourse(courses *mongo.Collection, course bson.M) {
 	}
 
 	fmt.Println("Found single document:", course)
-	fmt.Println("Successfully read.")
+}
+
+// Read all courses from db and output as json string
+func readAllCourses(courses *mongo.Collection, results []Course) {
+	cursor, err := courses.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Loop through all courses in cursor and append to results
+	for cursor.Next(context.Background()) {
+		var course Course
+		if err := cursor.Decode(&course); err != nil {
+			log.Fatal(err)
+		}
+		
+		results = append(results, course)
+	}
+
+	if err = cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cursor.Close(context.Background())
+
+	ret, err := json.Marshal(results)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Found all documents: ", string(ret))
 }
